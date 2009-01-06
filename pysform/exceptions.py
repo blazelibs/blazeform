e5b7b487bc5149b140dc84c425342291a410c4bd
Randy Syring <rsyring@gmail.com>
2009-01-06 05:00:40 +0000

    If ElementInvalid is thrown inside a custom form validator, that exception is caught and suppressed since the form will be invalid anyway.  Doing this makes writing custom validators straight forward.  If more fine-grained control is desired, trap the exception yourself.

--- pysform/exceptions.py
+++ pysform/exceptions.py
@@ -1,5 +1,6 @@
 
 class ElementInvalid(Exception):
+    """ raised when .value is accessed on an element but the element is not valid """
     def __init__(self, label):
         desc = '"value" attribute accessed, but element "%s" is invalid' % label
         Exception.__init__(self, desc)
