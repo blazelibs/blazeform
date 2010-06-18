4f731d2fa1059164a8fbc63f3c4c6533baf880fc
Randy Syring <randy.syring@level12.io>
2010-06-17 22:22:43 -0400

    committing fix for bug in SelectElement.render_static

---
+++ tests/test_processors.py
@@ -0,0 +1,12 @@
+from formencode import Invalid
+from formencode.validators import MaxLength
+
+def test_maxlength_bug_fix():
+    assert MaxLength._messages['__buggy_toolong'] == "Enter a value less than %(maxLength)i characters long", 'looks like formencode may have fixed the MaxLength message bug'
+    ml = MaxLength(5)
+    ml.to_python('12345')
+    try:
+        ml.to_python('123456')
+        assert False, 'expected exception'
+    except Invalid, e:
+        assert str(e) == 'Enter a value not greater than 5 characters long'
