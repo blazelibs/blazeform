f5772059f2751eca3e414f6c5361994c49f28b8e
Bill Adams <bladams@users.noreply.github.com>
2014-10-19 20:17:37 -0400

    refactor setting submitted values for convenience is subclasses

---
+++ tests/renderers/withaction.py
@@ -0,0 +1,7 @@
+import datetime
+from blazeform.form import Form
+
+class TestForm(Form):
+    def __init__(self):
+        Form.__init__(self, 'withactionform', action='/submitto')
+        el = self.add_text('text', 'Text')
---
+++ tests/test_exceptions.py
@@ -0,0 +1,9 @@
+from blazeform.exceptions import ValueInvalid
+
+def test_invalid_str():
+    e = ValueInvalid('foo')
+    assert str(e) == 'foo'
+    
+def test_invalid_exc():
+    e = ValueInvalid(ValueError('foo'))
+    assert str(e) == 'foo'
